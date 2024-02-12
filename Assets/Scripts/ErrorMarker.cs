2e7ca94c3647915f61b3be149503aaa40258b207
Miro Aurela <miroaurela@gmail.com>
2024-02-12 17:59:31 +0200

    Made error marker for animals back visible after being turned off by accident.

--- Assets/Scripts/ErrorMarker.cs
+++ Assets/Scripts/ErrorMarker.cs
@@ -5,21 +5,22 @@ using UnityEngine;
 public class ErrorMarker : MonoBehaviour
 {
     private MeshRenderer meshRenderer;
+    private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         meshRenderer = GetComponent<MeshRenderer>();
-        if (meshRenderer != null)
-            meshRenderer.enabled = false;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        DisableMarker();
     }
     public void EnableMarker()
     {
-        if (meshRenderer != null)
-            meshRenderer.enabled = true;
+        if (meshRenderer != null) meshRenderer.enabled = true;
+        if (spriteRenderer != null) spriteRenderer.enabled = true;
     }
     public void DisableMarker()
     {
-        if (meshRenderer != null)
-            meshRenderer.enabled = false;
+        if (meshRenderer != null) meshRenderer.enabled = false;
+        if (spriteRenderer != null) spriteRenderer.enabled = false;
     }
 }
